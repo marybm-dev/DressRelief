efb020c403a27a47f6c2bb229185cc81a2f0b52a
Mary <mlmartinez85@gmail.com>
2016-12-10 13:59:39 -0800

    #1 basic setup of hamburger menu

--- bFashionIster/HamburgerViewController.swift
+++ bFashionIster/HamburgerViewController.swift
@@ -9,17 +9,77 @@
 import UIKit
 
 class HamburgerViewController: UIViewController {
-
+    
+    @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var contentView: UIView!
+    
+    @IBOutlet weak var leftMarginConstraint: NSLayoutConstraint!
+    var originalLeftMargin: CGFloat!
+    
+    var menuViewController: UIViewController! {
+        didSet {
+            view.layoutIfNeeded()
+            menuView.addSubview(menuViewController.view)
+        }
+    }
+    
+    var contentViewController:  UIViewController! {
+        didSet(oldContentViewController) {
+            view.layoutIfNeeded()
+            
+            if oldContentViewController != nil {
+                oldContentViewController.willMove(toParentViewController: nil)
+                oldContentViewController.view.removeFromSuperview()
+                oldContentViewController.didMove(toParentViewController: nil)
+            }
+            
+            contentViewController.willMove(toParentViewController: self)
+            contentView.addSubview(contentViewController.view)
+            contentViewController.didMove(toParentViewController: self)
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                self.leftMarginConstraint.constant = 0
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
+    @IBAction func onPanGesture(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        let velocity = sender.velocity(in: view)
+        
+        switch sender.state {
+        case .began:
+            originalLeftMargin = leftMarginConstraint.constant
+            
+        case .changed:
+            leftMarginConstraint.constant = originalLeftMargin + translation.x
+            
+        case .ended:
+            UIView.animate(withDuration: 0.3, animations: {
+                if velocity.x > 0 { // opening
+                    self.leftMarginConstraint.constant = self.view.frame.size.width - 50
+                } else {            // closing
+                    self.leftMarginConstraint.constant = 0
+                }
+                self.view.layoutIfNeeded()
+            })
+            
+        default:
+            print("default")
+            
+        }
+    }
 
 }
 
