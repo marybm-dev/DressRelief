85f6a8caaf430e624acc7794ba35ecd74cedf237
Mary <mlmartinez85@gmail.com>
2017-02-07 08:53:02 -0800

    Update app icon

--- bFashionIster/AppDelegate.swift
+++ bFashionIster/AppDelegate.swift
@@ -31,6 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = Helper.initTabBarController()
         window?.makeKeyAndVisible()
+        window?.tintColor = .white
         
         return true
     }
