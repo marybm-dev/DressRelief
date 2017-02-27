2af508adf47eb638698650bd14afb3d8f0ed73fd
Mary <mlmartinez85@gmail.com>
2017-02-26 16:02:41 -0800

    Replace var with let

--- bFashionIster/AppDelegate.swift
+++ bFashionIster/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         Fabric.with([Crashlytics.self])
 
         
-        var config = Realm.Configuration(
+        let config = Realm.Configuration(
             // Set the new schema version. This must be greater than the previously used
             // version (if you've never set a schema version before, the version is 0).
             schemaVersion: 2,
